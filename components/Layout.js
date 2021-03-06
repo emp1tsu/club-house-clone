6ef4bdc9106a00e85d3366da4bf3c3f3042cc103
emp1tsu <oneok0813@gmail.com>
2021-03-07 00:05:23 +0900

    add Index page

---
+++ components/Layout.js
@@ -0,0 +1,44 @@
+export default function Layout({ children }) {
+  return (
+    <div className="container">
+      <div className="app">{children}</div>
+      <style jsx>{`
+        .container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 2em;
+          width: 100%;
+          heigh: 100vh;
+        }
+        .app {
+          min-width: 320px;
+          backdrop-filter: blur(20px);
+          background: rgba(16 18 27 / 40%);
+          border-radius: 14px;
+        }
+      `}</style>
+      <style jsx global>{`
+        :root {
+          --active-color: #fefffe;
+          --dark-bg: #14162b;
+        }
+        body {
+          font-family: "Poppins", sans-serif;
+          background: #355c7d;
+          background: -webkit-linear-gradient(
+            to right,
+            #355c7d,
+            #6c5b7b,
+            #c06c84
+          );
+          background: linear-gradient(to right, #355c7d, #6c5b7b, #c06c84);
+          color: var(--active-color);
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,156 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState, useMemo } from "react";
+import { useRouter } from "next/router";
+import uuid from "uuid-random";
+import {
+  createRoom as dbCreateRoom,
+  useFirestoreRooms,
+} from "../hooks/useFirestore";
 
-export default function Home() {
+import Layout from "../components/Layout";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import Heading from "../components/Heading";
+import RoomList from "../components/RoomList";
+
+import config from "../config";
+
+export default function Index() {
+  const router = useRouter();
+  const [userName, setUserName] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [createFormError, setCreateFormError] = useState(false);
+  const [micAccess, setMicAccess] = useState(false);
+
+  const [rooms] = useFirestoreRooms();
+
+  function requestMicAccess() {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        setMicAccess("granted");
+      })
+      .catch((err) => {
+        setMicAccess("denied");
+      });
+  }
+
+  useEffect(() => {
+    navigator.permissions
+      .query({ name: "microphone" })
+      .then((permissionStatus) => {
+        setMicAccess(permissionStatus.state);
+      })
+      .catch(() => {});
+  }, []);
+
+  const exploreRooms = useMemo(() => {
+    const now = +new Date() / 1000;
+    return rooms
+      .filter((room) => room.lastPing)
+      .filter((room) => now - room.lastPing.seconds < 30);
+  }, [rooms]);
+
+  function validForm() {
+    if (userName.trim().length < 3) {
+      setCreateFormError("名前は3文字以上入力してください。");
+      return false;
+    }
+    if (roomName.trim().length < 3) {
+      setCreateFormError("ルーム名は3文字以上入力してください。");
+      return false;
+    }
+    setCreateFormError(false);
+    return true;
+  }
+
+  function createRoom() {
+    if (!validForm()) return;
+    const roomId = uuid();
+    dbCreateRoom(roomId, { roomId, roomName, userName });
+    router.push(
+      {
+        pathname: "/cast/[roomId]",
+        as: `/cast/${roomId}`,
+        query: {
+          roomId,
+          roomName,
+          userName,
+        },
+      },
+      `/cast/${roomId}`
+    );
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+    <Layout>
+      <div style={{ padding: 20 }}>
+        <div className="spacing">
+          <h1
+            style={{
+              transform: "rotate(25deg)",
+              textAlign: "center",
+              fontSize: 60,
+            }}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            📢
+          </h1>
+          <Heading size={2}>Host Room</Heading>
+          <div>
+            <Input
+              placeholder="名前"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div>
+            <Input
+              placeholder="ルーム名"
+              onChange={(e) => setRoomName(e.target.value)}
+            />
+          </div>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        {createFormError && <div className="error">{createFormError}</div>}
+        <div style={{ marginTop: 20 }}>
+          <Button
+            success={micAccess === "granted"}
+            disabled={micAccess === "granted"}
+            fullWidth
+            onClick={requestMicAccess}
+          >
+            マイクを許可する
+          </Button>
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <Button
+            outline={micAccess !== "granted"}
+            disabled={micAccess !== "granted"}
+            big
+            fullWidth
+            onClick={createRoom}
+          >
+            部屋を作成
+          </Button>
+        </div>
+        {config.firebase.enabled && (
+          <div className="spacing" style={{ marginTop: 30 }}>
+            <Heading size={2}>最新の部屋</Heading>
+            {exploreRooms.length === 0 && (
+              <div>利用可能な部屋はありません。</div>
+            )}
+            <RoomList rooms={exploreRooms} />
+          </div>
+        )}
+      </div>
+      <style jsx>{`
+        .spacing > * {
+          margin-top: 10px;
+        }
+        .error {
+          font-size: 12px;
+          text-align: center;
+          margin: 6px 0;
+        }
+      `}</style>
+    </Layout>
+  );
 }
